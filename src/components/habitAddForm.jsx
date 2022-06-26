b37e2a5d28c7265cbb447ff92091ff85b9864691
hwajj <azamwara@gmail.com>
2022-06-27 02:23:58 +0900

    css 수정

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,6 +1,9 @@
 import React, { PureComponent } from 'react';
-
+//habitAddForm은 state따로 없고 onAdd를 props로 받는다
+//app에서 전달된 HandleAdd는 클래스 한번 만들어질때 할당된 뒤로 다시업데이트 되지 않는 함수
+//~ 리렌더 되지 않음 ~
 class HabitAddForm extends PureComponent {
+  //PureComponent 얕은비교해서 바뀌면 리렌더
   inputRef = React.createRef();
   formRef = React.createRef();
 
@@ -9,10 +12,12 @@ class HabitAddForm extends PureComponent {
     const name = this.inputRef.current.value;
     name && this.props.onAdd(name);
     // this.inputRef.current.value= '';
+    //둘중 하나
     this.formRef.current.reset();
   };
 
   render() {
+    console.log('habitAddForm');
     return (
       <form ref={this.formRef} className='add-form' onSubmit={this.onSubmit}>
         <input
