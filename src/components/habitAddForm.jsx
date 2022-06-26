d240c4f91f3f04f4c1e3583aae521f6b13f320e5
hwajj <azamwara@gmail.com>
2022-06-27 07:43:34 +0900

    function 컴포넌트로 바꾸기, memo 사용

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,13 +1,12 @@
 import React, { PureComponent } from 'react';
-//habitAddForm은 state따로 없고 onAdd를 props로 받는다
-//app에서 전달된 HandleAdd는 클래스 한번 만들어질때 할당된 뒤로 다시업데이트 되지 않는 함수
-//~ 리렌더 되지 않음 ~
-class HabitAddForm extends PureComponent {
-  //PureComponent 얕은비교해서 바뀌면 리렌더
-  inputRef = React.createRef();
-  formRef = React.createRef();
-
-  onSubmit = (event) => {
+import { memo } from 'react/cjs/react.production.min';
+// habitAddForm은 state따로 없고 onAdd를 props로 받는다
+// app에서 전달된 HandleAdd는 클래스 한번 만들어질때 할당된 뒤로 다시업데이트 되지 않는 함수
+// (PureComponent이거나 memo 사용했을때 , 얕은비교해서 바뀐것 없으므로) 리렌더 되지 않음
+const HabitAddForm = memo((props) => {
+  const inputRef = React.createRef();
+  const formRef = React.createRef();
+  const onSubmit = (event) => {
     event.preventDefault();
     const name = this.inputRef.current.value;
     name && this.props.onAdd(name);
@@ -16,20 +15,16 @@ class HabitAddForm extends PureComponent {
     this.formRef.current.reset();
   };
 
-  render() {
-    console.log('habitAddForm');
-    return (
-      <form ref={this.formRef} className='add-form' onSubmit={this.onSubmit}>
-        <input
-          ref={this.inputRef}
-          type='text'
-          className='add-input'
-          placeholder='Habit'
-        />
-        <button className='add-button'>Add</button>
-      </form>
-    );
-  }
-}
-
+  return (
+    <form ref={formRef} className='add-form' onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type='text'
+        className='add-input'
+        placeholder='Habit'
+      />
+      <button className='add-button'>Add</button>
+    </form>
+  );
+});
 export default HabitAddForm;
